a870c6db4ce9036da879b32169f67fc9c4624d19
jplethier <jplethier@gmail.com>
2010-12-15 19:01:51 -0200

    páginas estáticas

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                    :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
 
   describe "GET 'contact'" do
@@ -16,4 +24,12 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  
 end
